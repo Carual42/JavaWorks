67cacb7917574cb173f1445fb66e0ae3b4f104e3
Alex Carufel <Carual42@gmail.com>
2023-03-07 15:29:04 -0600

    println

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     ystem.out.println("Let's play hide and seek.");
     System.out.print("Three...");
     System.out.print("Two...");
+    ystem.out.println("One...");
+    System.out.println("Ready or not, here I come!");
   }
 }

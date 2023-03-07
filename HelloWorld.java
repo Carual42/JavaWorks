793731dde48b6fe8145f5f6373af7729c7a83f4c
Alex Carufel <Carual42@gmail.com>
2023-03-07 15:27:51 -0600

    print

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello Alex");
+    ystem.out.println("Let's play hide and seek.");
+    System.out.print("Three...");
+    System.out.print("Two...");
   }
 }

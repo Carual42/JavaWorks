584caed276aafdbde08bbc2f550e352143da097b
Alex Carufel <Carual42@gmail.com>
2023-03-08 21:38:27 -0600

    deleted world class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,3 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-
-  // outputs the following string
-    System.out.println("Hello Alex");
-    ystem.out.println("Let's play hide and seek.");
-    System.out.print("Three...");
-    System.out.print("Two...");
-    ystem.out.println("One...");
-    System.out.println("Ready or not, here I come!");
-  }
-}
 
 public class Compiling {
   public static void main(String[] args) {

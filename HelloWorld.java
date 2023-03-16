f51d6f636b6b197525370cc44edbf443b6ff5299
Alex Carufel <Carual42@gmail.com>
2023-03-16 16:20:17 -0500

    int

--- HelloWorld.java
+++ HelloWorld.java
@@ -19,3 +19,18 @@ public class Creator {
     System.out.println(yearCreated);
 	}
 }
+
+//This is the class declaration
+public class CountComment {
+  //This is the main method that runs when you compile
+	public static void main(String[] args) {
+    //This is where you will define your variable
+    int numComments = 6;
+    //This is where you will print your variable
+    System.out.println(numComments);
+	}
+  
+  //This is the end of the class
+}
+
+//This is outside the class

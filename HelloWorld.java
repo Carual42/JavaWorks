5f3f4ff809b0011d3ed2ab1de482c40cfefa5eb6
Alex Carufel <Carual42@gmail.com>
2023-03-16 16:41:56 -0500

    grading

--- HelloWorld.java
+++ HelloWorld.java
@@ -48,3 +48,9 @@ public class Booleans {
     System.out.println(intsCanHoldDecimals);
 	}
 }
+public class Char {
+	public static void main(String[] args) {   
+char expectedGrade = 'A';
+System.out.println(expectedGrade);
+	}
+}

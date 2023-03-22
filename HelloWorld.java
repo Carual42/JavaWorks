aed57182bfd09a5faa01d04c41b3ede54954aabb
Alex Carufel <Carual42@gmail.com>
2023-03-22 18:48:30 -0500

    bug infestation

--- HelloWorld.java
+++ HelloWorld.java
@@ -61,3 +61,12 @@ public class Song {
   System.out.println(openingLyrics);
 	}
 }
+public class Mess {
+	public static void main(String[] args) {   
+		String year = 2001;
+    double title = "Shrek";
+    int genre = 'C';
+    boolean runtime = 1.58;
+    char isPG = true;
+	}
+}

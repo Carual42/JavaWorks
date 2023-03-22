e6939d2a32928193e649cd833fd74eb9e12f5b63
Alex Carufel <Carual42@gmail.com>
2023-03-22 18:50:17 -0500

    int bug fix

--- HelloWorld.java
+++ HelloWorld.java
@@ -63,7 +63,7 @@ public class Song {
 }
 public class Mess {
 	public static void main(String[] args) {   
-		String year = 2001;
+		int year = 2001;
     double title = "Shrek";
     int genre = 'C';
     boolean runtime = 1.58;

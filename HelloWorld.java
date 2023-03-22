3598aa9c24f5c8a6f6855906c3f7525e3516c446
Alex Carufel <Carual42@gmail.com>
2023-03-22 18:52:15 -0500

    char double boolean bug fix

--- HelloWorld.java
+++ HelloWorld.java
@@ -65,8 +65,8 @@ public class Mess {
 	public static void main(String[] args) {   
 		int year = 2001;
     String title = "Shrek";
-    int genre = 'C';
-    boolean runtime = 1.58;
-    char isPG = true;
+    char genre = 'C';
+    double runtime = 1.58;
+    boolean isPG = true;
 	}
 }

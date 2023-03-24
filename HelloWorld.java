f9f8dce99d1379d2eba066edcaa6d73b71b29e5e
Alex Carufel <Carual42@gmail.com>
2023-03-23 20:05:01 -0500

    var manipulation

--- HelloWorld.java
+++ HelloWorld.java
@@ -70,3 +70,12 @@ public class Mess {
     boolean isPG = true;
 	}
 }
+
+public class PlusAndMinus {
+	public static void main(String[] args) {   
+		int zebrasInZoo = 8;
+    int giraffesInZoo = 4;
+    int animalsInZoo = zebrasInZoo + giraffesInZoo;
+    System.out.println(animalsInZoo);
+	}
+}

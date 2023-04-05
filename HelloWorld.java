d4d4a54938cb83ac93580407e4be509603b70603
Alex Carufel <Carual42@gmail.com>
2023-04-05 17:28:03 -0500

    remainder print

--- HelloWorld.java
+++ HelloWorld.java
@@ -96,6 +96,7 @@ public class MultAndDivide {
 public class Modulo {
 	public static void main(String[] args) {   
 		int students = 26;
-    
+    int leftOut = students % 3;
+    System.out.println(leftOut);
 	}
 }
